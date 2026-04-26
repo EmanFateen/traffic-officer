47c3fab8e1eb67a25eb0e55b47ee688eb960f089
Eman Fateen <emanfateen@gmail.com>
2026-04-26 16:23:32 +0300

    chore: implement rateLimitKeyFactory

--- src/Domain/Service/RateLimitKeyFactory.ts
+++ src/Domain/Service/RateLimitKeyFactory.ts
@@ -1,5 +1,14 @@
 import {UserIdentity} from "../types.ts";
+import {rateLimitKey} from "./RateLimitKey.ts";
 
-export function rateLimitKeyFactory(userIdentity: UserIdentity): {} {
-
+export function rateLimitKeyFactory(userIdentity: UserIdentity): {
+    apikey: string;
+    ip: string;
+    tenant: string;
+} {
+    return {
+        apikey: rateLimitKey("user").ownedBy(userIdentity.apiKey),
+        ip: rateLimitKey("ip").ownedBy(userIdentity.ip),
+        tenant: rateLimitKey("tenant").ownedBy(userIdentity.tenant),
+    };
 }
