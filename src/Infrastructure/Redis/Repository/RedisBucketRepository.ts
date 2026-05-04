dd69b4e3735cea26e51d20eb005df314704a6300
Eman Fateen <emanfateen@gmail.com>
2026-05-04 15:04:48 +0300

    chore: rename bucketState to state

--- src/Infrastructure/Redis/Repository/RedisBucketRepository.ts
+++ src/Infrastructure/Redis/Repository/RedisBucketRepository.ts
@@ -1,21 +1,21 @@
 import type { BucketRepository } from "../../../Domain/Repository/BucketRepository.ts";
-import type { TokenBucketState } from "../../../Domain/types.ts";
 import type { RedisClient } from "../Client/getClient.ts";
+import {TokenBucketState} from "../../../Domain/Algorithm/types.ts";
 
 export class RedisBucketRepository implements BucketRepository {
     constructor(private readonly redisClient: RedisClient) {}
 
     async get(key: string): Promise<TokenBucketState | null> {
-        const bucketState = await this.redisClient.get(key);
+        const state = await this.redisClient.get(key);
 
-        if (bucketState === null) {
+        if (state === null) {
             return null;
         }
 
-        return JSON.parse(bucketState) as TokenBucketState;
+        return JSON.parse(state) as TokenBucketState;
     }
 
-    async set(key: string, bucketState: TokenBucketState): Promise<void> {
-        await this.redisClient.set(key, JSON.stringify(bucketState));
+    async set(key: string, state: TokenBucketState): Promise<void> {
+        await this.redisClient.set(key, JSON.stringify(state));
     }
 }
