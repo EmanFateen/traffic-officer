97aaca23bfeff63e5787db0eacb5ee0d5fa307ad
Eman Fateen <emanfateen@gmail.com>
2026-05-05 13:15:21 +0300

    Add api key limit service path
    # Conflicts:
    #	src/Domain/Service/LimitService.test.ts
    #	src/Domain/Service/LimitService.ts

--- src/Domain/Service/LimitService.ts
+++ src/Domain/Service/LimitService.ts
@@ -3,81 +3,65 @@ import { TokenBucket } from "../Algorithm/TokenBucket.ts";
 import { StateRepositoryInterface } from "../Repository/StateRepositoryInterface.ts";
 import {
     Algorithm,
-    IdentifierBuilder,
+    Decision,
     LimitConfig,
     LimitDecisions,
     UserIdentity,
 } from "../types.ts";
-import { stateIdentifierFactory } from "./StateIdentifierFactory.ts";
-import {rateLimiterAlgorithmFactory} from "../Algorithm/RateLimiterAlgorithmFactory.ts";
+import {
+    IdentifierBuilderFactory,
+    stateIdentifierFactory,
+} from "./StateIdentifierFactory.ts";
 
-export class LimitService<State, Config> {
+export class LimitService<Client, State, Config> {
     constructor(
-        private readonly stateRepository: StateRepositoryInterface<State>,
-        private readonly identifierBuilder: IdentifierBuilder,
-        private readonly limitingAlgorithm: RateLimitingAlgorithm<State, Config>,
+        private readonly stateRepository: StateRepositoryInterface<Client, State>,
+        private readonly client: Client,
+        private readonly identifierBuilderFactory: IdentifierBuilderFactory,
     ) {}
 
     async limit(
         userIdentity: UserIdentity,
         config: LimitConfig<Config>,
+        algorithm: Algorithm,
         requestedAtInMs: number,
     ): Promise<LimitDecisions<State>> {
         const stateIdentifiers = stateIdentifierFactory(
-            this.identifierBuilder,
+            this.identifierBuilderFactory,
             userIdentity,
         );
-
-
-        const state = await this.stateRepository.findOneBy(
+        const tokenBucketAlgorithm = new TokenBucket() as unknown as RateLimitingAlgorithm<
+            State,
+            Config
+        >;
+        const apiKeyDecision = await this.decide(
+            tokenBucketAlgorithm,
             stateIdentifiers.apikey,
-        );
-        const apiKeyDecision = this.limitingAlgorithm.limit(
-            state,
             config.apiKey,
             requestedAtInMs,
-        );
-        await this.stateRepository.save(
-            stateIdentifiers.apikey,
-            apiKeyDecision.nextState,
         );
         const limitDecisions: LimitDecisions<State> = {
             apiKey: apiKeyDecision,
         };
 
-
-        if (stateIdentifiers.ip !== undefined && config.ip !== undefined) {
-            const state = await this.stateRepository.findOneBy(stateIdentifiers.ip);
-            limitDecisions.ip = this.limitingAlgorithm.limit(
-                state,
-                config.ip,
-                requestedAtInMs,
-            );
-            await this.stateRepository.save(
-                stateIdentifiers.ip,
-                limitDecisions.ip.nextState,
-            );
-        }
-
-
-        if (
-            stateIdentifiers.tenant !== undefined &&
-            config.tenant !== undefined
-        ) {
-            const state = await this.stateRepository.findOneBy(
-                stateIdentifiers.tenant,
-            );
-            limitDecisions.tenant = this.limitingAlgorithm.limit(
-                state,
-                config.tenant,
-                requestedAtInMs,
-            );
-            await this.stateRepository.save(
-                stateIdentifiers.tenant,
-                limitDecisions.tenant.nextState,
-            );
-        }
-
         return limitDecisions;
+    }
+
+    private async decide(
+        tokenBucket: RateLimitingAlgorithm<State, Config>,
+        stateIdentifier: string,
+        config: Config,
+        requestedAtInMs: number,
+    ): Promise<Decision<State>> {
+        const state = await this.stateRepository.get(this.client, stateIdentifier);
+        const decision = tokenBucket.limit(state as State, config, requestedAtInMs);
+
+        await this.stateRepository.set(
+            this.client,
+            stateIdentifier,
+            decision.nextState,
+        );
+
+        return decision;
     }
 }
