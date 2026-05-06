7fdff00c1a11ac05dd50af073384f14277d09acf
Eman Fateen <emanfateen@gmail.com>
2026-05-06 14:22:42 +0300

    chore: introduce RateLimiterAlgorithmFactory

--- src/Domain/Algorithm/RateLimiterAlgorithmFactory.ts
+++ src/Domain/Algorithm/RateLimiterAlgorithmFactory.ts
@@ -1,9 +1,13 @@
-import {AlgorithmName} from "../types.ts";
-import {RateLimitingAlgorithm} from "./RateLimitingAlgorithm.ts";
 import {TokenBucket} from "./TokenBucket.ts";
+import {RateLimitingAlgorithm} from "./RateLimitingAlgorithm.ts";
+import {TokenBucketConfig, TokenBucketState} from "./types.ts";
 
-export function rateLimiterAlgorithmFactory<State,Config>(algorithmName: AlgorithmName): RateLimitingAlgorithm<State,Config>{
-    if(algorithmName === 'TokenBucket') return new TokenBucket() as unknown as RateLimitingAlgorithm<State, Config>;
+type AlgorithmsMap = {
+    TokenBucket: RateLimitingAlgorithm<TokenBucketState, TokenBucketConfig>;
+}
+
+export function rateLimiterAlgorithmFactory<T extends keyof AlgorithmsMap>(algorithmName: T): AlgorithmsMap[T]{
+    if(algorithmName === 'TokenBucket') return new TokenBucket() as AlgorithmsMap[T];
 
     throw new Error(`${algorithmName} is unsupported algorithm.`);
 }
