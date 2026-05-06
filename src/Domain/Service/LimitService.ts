b14d796ba5f1588a216d2f5420e615e794fd78f4
Eman Fateen <emanfateen@gmail.com>
2026-05-06 13:12:18 +0300

    Inline limit state persistence

--- src/Domain/Service/LimitService.ts
+++ src/Domain/Service/LimitService.ts
@@ -3,7 +3,6 @@ import { TokenBucket } from "../Algorithm/TokenBucket.ts";
 import { StateRepositoryInterface } from "../Repository/StateRepositoryInterface.ts";
 import {
     Algorithm,
-    Decision,
     IdentifierBuilder,
     LimitConfig,
     LimitDecisions,
@@ -27,26 +26,38 @@ export class LimitService<State, Config> {
             this.identifierBuilder,
             userIdentity,
         );
-        const tokenBucketAlgorithm = new TokenBucket() as unknown as RateLimitingAlgorithm<
+        const limitingAlgorithm = new TokenBucket() as unknown as RateLimitingAlgorithm<
             State,
             Config
         >;
-        const apiKeyDecision = await this.decide(
-            tokenBucketAlgorithm,
+
+        const state = await this.stateRepository.findOneBy(
             stateIdentifiers.apikey,
+        );
+        const apiKeyDecision = limitingAlgorithm.limit(
+            state,
             config.apiKey,
             requestedAtInMs,
         );
+        await this.stateRepository.save(
+            stateIdentifiers.apikey,
+            apiKeyDecision.nextState,
+        );
+
         const limitDecisions: LimitDecisions<State> = {
             apiKey: apiKeyDecision,
         };
 
         if (stateIdentifiers.ip !== undefined && config.ip !== undefined) {
-            limitDecisions.ip = await this.decide(
-                tokenBucketAlgorithm,
-                stateIdentifiers.ip,
+            const state = await this.stateRepository.findOneBy(stateIdentifiers.ip);
+            limitDecisions.ip = limitingAlgorithm.limit(
+                state,
                 config.ip,
                 requestedAtInMs,
+            );
+            await this.stateRepository.save(
+                stateIdentifiers.ip,
+                limitDecisions.ip.nextState,
             );
         }
 
@@ -54,28 +65,20 @@ export class LimitService<State, Config> {
             stateIdentifiers.tenant !== undefined &&
             config.tenant !== undefined
         ) {
-            limitDecisions.tenant = await this.decide(
-                tokenBucketAlgorithm,
+            const state = await this.stateRepository.findOneBy(
                 stateIdentifiers.tenant,
+            );
+            limitDecisions.tenant = limitingAlgorithm.limit(
+                state,
                 config.tenant,
                 requestedAtInMs,
+            );
+            await this.stateRepository.save(
+                stateIdentifiers.tenant,
+                limitDecisions.tenant.nextState,
             );
         }
 
         return limitDecisions;
-    }
-
-    private async decide(
-        tokenBucket: RateLimitingAlgorithm<State, Config>,
-        stateIdentifier: string,
-        config: Config,
-        requestedAtInMs: number,
-    ): Promise<Decision<State>> {
-        const state = await this.stateRepository.findOneBy(stateIdentifier);
-        const decision = tokenBucket.limit(state, config, requestedAtInMs);
-
-        await this.stateRepository.save(stateIdentifier, decision.nextState);
-
-        return decision;
     }
 }
