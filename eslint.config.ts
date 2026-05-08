6331194f99631edf7b515dedfb75d78082d72bb5
Eman Fateen <emanfateen@gmail.com>
2026-05-08 11:48:32 +0300

    chore: add prettier to eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
   { ignores: ['dist/**', 'node_modules/**', 'coverage/**'] },
@@ -12,4 +13,5 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+  prettier,
 ]);
