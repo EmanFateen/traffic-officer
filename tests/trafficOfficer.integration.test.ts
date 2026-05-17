a0091bb0f92ac422321920b768bee949cfe4e6f5
Eman Fateen <emanfateen@gmail.com>
2026-05-17 14:02:31 +0300

    chore: introduce trafficOfficer file

--- tests/trafficOfficer.integration.test.ts
+++ tests/trafficOfficer.integration.test.ts
@@ -6,7 +6,7 @@ import {
   closeClient,
   getClient,
 } from "../src/Infrastructure/Cache/Redis/Client/getClient.ts";
-import { createTrafficOfficer } from "../src";
+import { createTrafficOfficer } from "../src/trafficOfficer";
 
 describe("traffic officer public API", () => {
   let redisKeysToDelete: string[] = [];
