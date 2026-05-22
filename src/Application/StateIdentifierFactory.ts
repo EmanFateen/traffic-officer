a839fc9623a9fb28f2676b3150e3e6cb8047aaaa
Eman Fateen <emanfateen@gmail.com>
2026-05-22 17:46:37 +0300

    wip

--- src/Application/StateIdentifierFactory.ts
+++ src/Application/StateIdentifierFactory.ts
@@ -3,10 +3,6 @@ import { IdentifierBuilder, Identities } from "./Identities.ts";
 import { StateIdentifiers } from "../Domain/StateIdentifiers.ts";
 
 export function stateIdentifierFactory(identifierBuilder: IdentifierBuilder, identities: Identities): StateIdentifiers {
-  if (!identities.apiKey) {
-    throw new Error("apiKey is required to generate the identifiers");
-  }
-
   const keys: StateIdentifiers = {
     apiKey: identifierBuilder("apiKey").ownedBy(identities.apiKey),
   };
