16f96dfdeee810bbf477f671dcef53bca393cd46
Eman Fateen <emanfateen@gmail.com>
2026-05-22 17:32:06 +0300

    wip

--- src/Domain/Algorithm/TokenBucket.ts
+++ src/Domain/Algorithm/TokenBucket.ts
@@ -21,48 +21,53 @@ export class TokenBucket implements AlgorithmInterface<TokenBucketState, TokenBu
     policy: TokenBucketPolicy,
     requestedAtInMs: number,
   ): Decision<TokenBucketState> {
-    const currentBucketState: TokenBucketState = state ?? {
-      tokensCount: policy.bucketCapacityLimit,
-      lastUpdatedAtInMs: 0,
-    };
+    const currentBucketState = state ?? { tokensCount: policy.bucketCapacityLimit, lastUpdatedAtInMs: 0};
+    const updatedAt = Math.max(requestedAtInMs, currentBucketState.lastUpdatedAtInMs);
 
-    const refilledTokens: number = refillTokens();
+    const refilledTokens: number = this.refillTokens(requestedAtInMs, currentBucketState, policy);
 
     const remainingTokens: number = Math.round((refilledTokens - CONSUMPTION_AMOUNT) * 1000) / 1000;
 
-    return remainingTokens >= 0 ? allowed() : rejected();
+    return remainingTokens >= 0
+      ? this.allowed(remainingTokens, updatedAt)
+      : this.rejected(remainingTokens, updatedAt, policy, refilledTokens);
+  }
 
-    function refillTokens(): number {
-      const elapsedTime: number = Math.max(0, requestedAtInMs - currentBucketState.lastUpdatedAtInMs);
-      const tokensCountCanBeRefiled: number = elapsedTime * (policy.refillRate.amount / policy.refillRate.perMs);
+  refillTokens(requestedAtInMs: number, currentBucketState: TokenBucketState, policy: TokenBucketPolicy): number {
+    const elapsedTime: number = Math.max(0, requestedAtInMs - currentBucketState.lastUpdatedAtInMs);
+    const tokensCountCanBeRefiled: number = elapsedTime * (policy.refillRate.amount / policy.refillRate.perMs);
 
-      return Math.min(policy.bucketCapacityLimit, currentBucketState.tokensCount + tokensCountCanBeRefiled);
-    }
+    return Math.min(policy.bucketCapacityLimit, currentBucketState.tokensCount + tokensCountCanBeRefiled);
+  }
 
-    function allowed() {
-      return {
-        allowed: true,
-        retryAfter: 0,
-        remaining: Math.floor(remainingTokens),
-        nextState: {
-          tokensCount: remainingTokens,
-          lastUpdatedAtInMs: Math.max(requestedAtInMs, currentBucketState.lastUpdatedAtInMs),
-        },
-        stateExpiresInMs: 0,
-      };
-    }
+  allowed(remainingTokens: number, updatedAt: number): Decision<TokenBucketState> {
+    return {
+      allowed: true,
+      retryAfter: 0,
+      remaining: Math.floor(remainingTokens),
+      nextState: {
+        tokensCount: remainingTokens,
+        lastUpdatedAtInMs: updatedAt,
+      },
+      stateExpiresInMs: 0,
+    };
+  }
 
-    function rejected() {
-      return {
-        allowed: false,
-        retryAfter: Math.ceil((remainingTokens * -1 * policy.refillRate.perMs) / policy.refillRate.amount),
-        remaining: 0,
-        nextState: {
-          tokensCount: Math.trunc(refilledTokens * 100) / 100,
-          lastUpdatedAtInMs: Math.max(requestedAtInMs, currentBucketState.lastUpdatedAtInMs),
-        },
-        stateExpiresInMs: 0,
-      };
-    }
+  rejected(
+    remainingTokens: number,
+    updatedAt: number,
+    policy: TokenBucketPolicy,
+    refilledTokens: number,
+  ): Decision<TokenBucketState> {
+    return {
+      allowed: false,
+      retryAfter: Math.ceil((remainingTokens * -1 * policy.refillRate.perMs) / policy.refillRate.amount),
+      remaining: 0,
+      nextState: {
+        tokensCount: Math.trunc(refilledTokens * 100) / 100,
+        lastUpdatedAtInMs: updatedAt,
+      },
+      stateExpiresInMs: 0,
+    };
   }
 }
